2a824e6a341c833c94210e5f1c0ec25e69e0530a
017Asd <115368231+017Asd@users.noreply.github.com>
2024-10-10 10:32:42 +0530

    First commmit

---
+++ 22f.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+    int age;
+    printf("Enter your age: ");  
+    scanf("%d",&age);
+if (age>=18) 
+{
+printf("You are eligible to vote");
+}
+//if (age<18)
+//{
+//	printf("You are underage to vote");
+//}
+else
+{
+	printf("You are underage to vote. .....GROW UP");
+}
+
+}
+//if ,if else, if else if, nested, switch
+
